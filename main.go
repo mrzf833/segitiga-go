ce04d2c5c70d77ba9aef49aa2bedc3b6897985a0
falaqzulfa <falaqzulfa@student.telkomuniversity.ac.id>
2024-07-08 18:06:44 +0700

    terbaru

--- main.go
+++ main.go
@@ -3,19 +3,62 @@ package main
 import "fmt"
 
 func main() {
-	jumlahSisi := 3
-	for i := 1; i <= jumlahSisi; i++ {
-		// cetak luarnya
-		for j := jumlahSisi; j > i; j-- {
-			fmt.Print(" ")
+	jumlahSisi := 10
+	//jumlahSisi *= 2
+	//jumlahSisi += 6
+
+	jumlahSisi *= 2
+	for i := jumlahSisi; i >= 1; i-- {
+		// pengecekan dari tengah
+		//for l := 0; l < l; l++ {
+		//	if jumlahSisi/i == 0 {
+		//		fmt.Println("*")
+		//	}
+		//}
+		start := i / 2
+		sisa := i % 2
+		start += sisa
+		if sisa != 1 {
+			continue
+		}
+		for l := 1; l <= jumlahSisi/2; l++ {
+			if start == l {
+				fmt.Print("*")
+			}
+
+			if l < start {
+				fmt.Print(" ")
+			}
+
+			//if start == l {
+			//	fmt.Print("*")
+			//} else {
+			//	fmt.Print(" ")
+			//}
+		}
+		for l := jumlahSisi / 2; l >= 1; l-- {
+			//if start != l {
+			//	fmt.Print(" ")
+			//} else {
+			//	fmt.Print("*")
+			//}
+
+			if start != l {
+				fmt.Print(" ")
+			}
+
+			if l > start {
+				fmt.Print("*")
+			}
 		}
 
-		// cetak dalamnya
-		for k := 1; k <= 2*i-1; k++ {
-			fmt.Print("1")
-		}
-		fmt.Println()
+		fmt.Println("")
 	}
+
+	//dataBagi2 := 9 / 2
+	//dataSisa := 9 % dataBagi2
+	//
+	//fmt.Println(dataSisa)
 }
 
 ////
